95906a2f656060e027f1a81d0de07ebd412633c8
Pankaj-kushwah1 <126949711+Pankaj-kushwah1@users.noreply.github.com>
2024-07-17 20:51:38 +0530

    apis for admin is added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import connectDB from "./src/connection.js";
 import env from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
-// import adminRoute from "./src/routes/adminRoute.js";
+import adminRoute from "./src/routes/adminRoute.js";
 import userRoute from "./src/routes/userRoute.js";
 import interestRoute from './src/routes/interestRoute.js';
 import friendsRoute from './src/routes/friendsRoute.js'
@@ -22,6 +22,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use("/api/v1/user", userRoute);
 app.use('/api/v1/interest',interestRoute);
 app.use('/api/v1/friends',friendsRoute);
+app.use('/api/v1/admin',adminRoute);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, (err) => {
