399b58885854ede256b4c44d5dbf2496cc6fe190
Pankaj-kushwah1 <126949711+Pankaj-kushwah1@users.noreply.github.com>
2024-07-18 10:52:01 +0530

    correction made in user controller for signup

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -34,13 +34,9 @@ export const userSignUp = async (req, res) => {
           .json({ success: false, message: `${field} is required` });
       }
     }
-    let existingUser= await User.findOne({email});
-    if(existingUser) return res.status(400).json({
-      success:false,
-      message:`A user is already registered with '${email}'`
-    })
-     existingUser= await User.findOne({phone});
-    if(existingUser) return res.status(400).json({
+    
+    let existingUser= await User.findOne({phone});
+    if(existingUser) return res.status(200).json({
       success:false,
       message:`A user is already registered with '${phone}'`
     })
