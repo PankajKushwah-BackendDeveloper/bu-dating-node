17f46c69f50587ecbcf8a2ff35b35ac8ffaad5ff
Pankaj-kushwah1 <126949711+Pankaj-kushwah1@users.noreply.github.com>
2024-07-25 00:01:47 +0530

    apis to update and delete tags has been added

--- src/routes/tagsRoute.js
+++ src/routes/tagsRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js';
-import { addFollwer, addTags, getAllTags } from '../controller/tagsController.js';
+import { addFollwer, addTags, deleteTag, getAllTags, updateTag } from '../controller/tagsController.js';
 
 const router = express.Router();
 
 router.post('/add-tag',requireSignIn,isAdmin,addTags);
 router.get('/get-all',getAllTags);
 router.post('/add-follower',requireSignIn,addFollwer);
+router.put('/update/:tagId',requireSignIn,isAdmin,updateTag);
+router.delete('/delete/:tagId',requireSignIn,isAdmin,deleteTag);
 export default router;
